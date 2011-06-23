b14139b18a4ced3bba4c69b3dfd0d6287c037431
Joe Hermaszewski <t-joherm@microsoft.com>
2011-06-23 15:26:07 +0100

    [!] Moved toolchain files to cmake/ and added w32 toolchain

--- cmake/w32-mingw.cmake
+++ cmake/w32-mingw.cmake
@@ -2,16 +2,16 @@
 SET(CMAKE_SYSTEM_NAME Windows)
  
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER gcc)
-SET(CMAKE_CXX_COMPILER g++)
-SET(CMAKE_RC_COMPILER windres)
+SET(CMAKE_C_COMPILER x86_64-pc-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER x86_64-pc-mingw32-g++)
+SET(CMAKE_RC_COMPILER x86_64-pc-mingw32-windres)
  
 # here is the target environment located
-if(WIN32)
-    SET(CMAKE_FIND_ROOT_PATH C:/MinGW/x86_64-w64-mingw32)
-else()
-    set(CMAKE_FIND_ROOT_PATH /usr/mingw/x86_64-w64-mingw32)
-endif()
+#if(WIN32)
+#    SET(CMAKE_FIND_ROOT_PATH /MinGW/x86_64-w64-mingw32)
+#else()
+    set(CMAKE_FIND_ROOT_PATH /usr/mingw/x86_64-pc-mingw32)
+#endif()
  
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
