a02475a855f785cde9bd0f88f6a04dfd85082440
Joe Hermaszewski <t-joherm@microsoft.com>
2011-06-24 14:07:16 +0100

    [+] linux toolchain and toolchain fixes

--- cmake/linux32-gcc.cmake
+++ cmake/linux32-gcc.cmake
@@ -1,14 +1,16 @@
 # the name of the target operating system
-SET(CMAKE_SYSTEM_NAME Windows)
+SET(CMAKE_SYSTEM_NAME Linux)
  
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+SET(CMAKE_C_COMPILER gcc)
+SET(CMAKE_CXX_COMPILER g++)
  
 # here is the target environment located
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+#set(CMAKE_FIND_ROOT_PATH /usr/i686-linux-gnu)
  
+set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS} -m32 -march=i686)
+set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -m32 -march=i686)
+
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
 # programs in the host environment
